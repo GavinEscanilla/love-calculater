a29032224603d80c542a3fc80329dcfe1c155307
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-01-23 03:06:42 -0500

    started

---
+++ script.js
@@ -0,0 +1,14 @@
+const name1 = document.getElementById('input1');
+const name2 = document.getElementById('input2');
+const  heart = document.getElementById('heart');
+const submit = document.getElementById('submit');
+const answer = document.getElementById('answer');
+const result = () =>{
+    const name1 = document.getElementById('input1');
+const name2 = document.getElementById('input2');
+    if(name1.value === '' && name2.value === ''){
+   answer.innerText = 'name';
+    }
+   
+}
+answer.innerText = '';
