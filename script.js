06f8daf0020108c0c4a07dc4b0da95276bdb89d9
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-01-23 04:04:47 -0500

    got the function to work and fixed textcontent alignment

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const name1 = document.getElementById('input1');
 const name2 = document.getElementById('input2');
 const  heart = document.getElementById('heart');
 const submit = document.getElementById('submit');
-const answer = document.getElementById('answer');
+const calculate = () => {
+    const answer = Math.floor(Math.random() * 101);
+    return answer;
+};
 const result = () =>{
     const name1 = document.getElementById('input1');
 const name2 = document.getElementById('input2');
     if(name1.value === '' && name2.value === ''){
-   answer.innerText = 'name';
+   heart.textContent= 'Please enter a names';
+    }else{
+   heart.textContent= `${calculate()}%`;
     }
    
 }
-answer.innerText = '';
+
